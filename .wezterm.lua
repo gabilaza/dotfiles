3aea498ce4a73db5a6cbca385b688ed845ef68f7
Gabriel LAZA <gabriellaza02@gmail.com>
2024-02-20 00:05:35 +0200

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- Changing the color scheme:
+config.color_scheme = "rose-pine"
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font "Fira Mono"
+
+-- config.window_background_opacity = 0.9
+
+-- and finally, return the configuration to wezterm
+return config
+
